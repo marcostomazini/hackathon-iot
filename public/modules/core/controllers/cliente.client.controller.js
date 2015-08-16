37741fed1e9f12b0d562c43d768c70de382e4de8
marcos.tomazini@gmail.com <marcos.tomazini@gmail.com>
2015-08-16 10:55:59 +0000

    prefeitura

--- public/modules/core/controllers/cliente.client.controller.js
+++ public/modules/core/controllers/cliente.client.controller.js
@@ -4,15 +4,13 @@ angular.module('core').controller('ClienteController', ['$scope', 'ChartData', '
     $interval(function () {
         $.getJSON("/api/sensor/caixa", function (data) {
             try {
-                $.getJSON("/api/sensor/caixa", function (data2) {
                     var dado = data[0];
                     var litros = parseInt(dado.valor);
                     var pctg = Math.floor(litros / 2000 * 100);
                     if (pctg > 100) pctg = 100;
                     $('#water').text(litros.toString() + " litros");
                     if (Math.abs(data[0].valor - data[10].valor) > 10)
-                        $('#water').toggleClass("animate").css({ height: pctg + "%" });
-                });
+                        $('#water').toggleClass("animate").css({ height: pctg + "%" });                
             }
             catch (ex) { }
         });
