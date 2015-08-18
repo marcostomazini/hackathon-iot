23720756d508b7a51b7fb5210aad457f29e31fe2
unknown <marcos.tomazini@gmail.com>
2015-08-18 13:30:26 -0300

    aaa

--- public/modules/core/controllers/app.client.controller.js
+++ public/modules/core/controllers/app.client.controller.js
@@ -36,6 +36,38 @@ angular.module('core').controller('AppController',
     $scope.authentication = Authentication;
     $scope.authentication.empresas = Empresas.query();    
 
+
+$scope.caixa = [];
+var stopTime;
+$scope.fightTime = function() {
+  // Don't start a new fight if we are already fighting
+  if ( angular.isDefined(stopTime) ) return;
+
+  stopTime = $interval(function () {
+    $scope.stopFightTime();
+     $.getJSON("/api/sensor/caixa", function (data) {        
+            try {
+              $scope.caixa = data;
+               $.getJSON("/api/sensor/caixa-historico", function (data2) {
+                $scope.fightTime();
+                  try {
+                    $scope.historico = data2             
+                  }
+                  catch (ex) { }
+              });
+            }
+            catch (ex) { }
+        });
+  }, 10000); 
+};
+
+$scope.stopFightTime = function() {
+  if (angular.isDefined(stopTime)) {
+    $interval.cancel(stopTime);
+    stopTime = undefined;
+  }
+};
+
     // Loading bar transition
     // ----------------------------------- 
     var thBar;
--- public/modules/core/controllers/cliente.client.controller.js
+++ public/modules/core/controllers/cliente.client.controller.js
@@ -1,40 +1,6 @@
 angular.module('core').controller('ClienteController', ['$scope', 'ChartData', '$timeout', '$interval', function ($scope, ChartData, $timeout, $interval) {
     'use strict';
 
-
-
-$scope.caixa = [];
-var stopTime;
-$scope.fightTime = function() {
-  // Don't start a new fight if we are already fighting
-  if ( angular.isDefined(stopTime) ) return;
-
-  stopTime = $interval(function () {
-    $scope.stopFightTime();
-     $.getJSON("/api/sensor/caixa", function (data) {
-        $scope.fightTime();
-            try {
-              $scope.caixa = data;
-
-               $.getJSON("/api/sensor/caixa-historico", function (data2) {
-                  try {
-                    $scope.historico = data2             
-                  }
-                  catch (ex) { }
-              });
-            }
-            catch (ex) { }
-        });
-  }, 10000); 
-};
-
-$scope.stopFightTime = function() {
-  if (angular.isDefined(stopTime)) {
-    $interval.cancel(stopTime);
-    stopTime = undefined;
-  }
-};
-
     $interval(function () {         
         var data = $scope.caixa;
 
